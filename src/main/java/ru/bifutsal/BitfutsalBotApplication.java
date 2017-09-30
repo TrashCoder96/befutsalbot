1227c8dc349bcbed27c9b16cad4d08a87478cec5
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2017-10-01 02:02:45 +0300

    init

---
+++ src/main/java/ru/bifutsal/BitfutsalBotApplication.java
@@ -0,0 +1,12 @@
+package ru.bifutsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitfutsalBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitfutsalBotApplication.class, args);
+	}
+}
